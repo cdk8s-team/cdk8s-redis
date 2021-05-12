0f8bf23a9492f6200dbb7281041e06eabd020c67
Elad Ben-Israel <benisrae@amazon.com>
2021-05-12 18:36:39 +0000

    upgrade projen

--- .projenrc.js
+++ .projenrc.js
@@ -2,17 +2,18 @@ const { JsiiProject, Semver } = require('projen');
 
 const project = new JsiiProject({
   name: 'cdk8s-redis',
-  jsiiVersion: Semver.caret('1.5.0'),
+  defaultReleaseBranch: 'master',
   description: 'redis constructs for cdk8s',
-  repository: 'https://github.com/eladb/cdk8s-redis.git',
+  repository: 'https://github.com/cdk8s-team/cdk8s-redis.git',
   authorName: 'Elad Ben-Israel',
   authorEmail: 'benisrae@amazon.com',
   stability: 'experimental',
-  peerDependencies: {
-    cdk8s: Semver.caret('0.20.0'),
-    constructs: Semver.caret('2.0.1'),
-  },
-  python: {
+  projenUpgradeSecret: 'PROJEN_GITHUB_TOKEN',
+  peerDeps: [
+    'cdk8s',
+    'constructs'
+  ],
+  publishToPypi: {
     distName: 'cdk8s-redis',
     module: 'cdk8s_redis'
   }
